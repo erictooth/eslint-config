967d5f1732b8616f99c20be62ef6039c20258216
Eric Mantooth <1023110+erictooth@users.noreply.github.com>
2019-05-26 10:51:50 -0700

    Initial commit

---
+++ esmodules.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  }
+};
---
+++ react.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    "@erictooth/esmodules",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier/react"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error"
+  }
+};
